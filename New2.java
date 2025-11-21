ec0d8d88aa070b02a8662d6e99b41282110bcd63
Java Student <amanrajput6727@gmail.com>
2025-11-21 13:51:18 +0530

    Add new Files

---
+++ New2.java
@@ -0,0 +1,8 @@
+public class New2 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        int sum=a+b;
+        System.out.println("Sum of Two Number is "+sum);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Aman-demo2
 This is a Demo for Git &amp; GitHub Class.
 
- 
+ # Teacher
+ Shradha Khapra
+
+ # Student 
+ Aman
